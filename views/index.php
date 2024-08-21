8bba64aa4ebdc988526b2c5e986d8cab6d8cca56
PinkiKoiri <pinkikoiri092@gmail.com>
2024-08-21 15:48:44 +0530

    table created

--- views/index.php
+++ views/index.php
@@ -1,18 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>TrackR:Inventory Management System</title>
+</head>
+
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th >Sl. No.</th>
+      <th >Asset Type (Active/Passive)</th>
+      <th >Serial No.</th>
+      <th >Delivery Date</th>
+      <th >Installation and Configuration Date</th>
+      <th >Warranty</th>
+      <th >Quantity</th>
+      <th >Purchase order ref. no</th>
+      <th >Location of physical installation</th>
+      <th >Status</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>1</td>
+      <td>Active</td>
+      <td>123</td>
+      <td>1</td>
+      <td>12.02.2010</td>
+      <td>20.02.2010</td>
+      <td>2 years</td>
+      <td>1</td>
+      <td>2341</td>
+      <td>Dept. of English</td>
+      <td>passive</td>
+
     </tr>
     <tr>
       <th scope="row">2</th>
@@ -27,3 +51,4 @@
     </tr>
   </tbody>
 </table>
+</html>
